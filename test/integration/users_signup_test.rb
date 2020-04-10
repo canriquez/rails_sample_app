cbf42c24be712e244035522f34da4f9c3242b08b
Carlos Anriquez <canriquez@gmail.com>
2020-04-10 13:07:21 -0300

    adding valid signup information test including flash presence on success

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,5 +16,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'div#error_explanation'
       assert_select 'div.alert'
     end
+
+      test "valid signup information" do
+        get signup_path
+        assert_difference 'User.count', 1 do
+          post users_path, params: {user: {name: "example User",
+                                            email: "user@example.com",
+                                            password:               "password",
+                                            password_confirmation:  "password"}}
+        end
+        follow_redirect!
+        assert_template 'users/show'
+        assert_select 'div.alert-success'  #testing is the flash has rendered
+        assert_not flash.nil?         #testing if flash present
+      end
+    
 end
 
